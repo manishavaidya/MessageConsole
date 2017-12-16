60cc8b40f5ddace3e305e6b2c4c7eee39b4cd697
Manisha <manisha.vaidya@gmail.com>
2017-12-16 15:32:30 -0600

    Initial commit

---
+++ MessageConsole/ConsumeAPI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageConsole
+{
+    public class ConsumeAPI
+    {
+       public async Task HelloWorldAPIGet(HttpClient myConsole, int id)
+        {
+            using (myConsole)
+            {
+                HttpResponseMessage res = await myConsole.GetAsync("api/HelloWorld/" + id);
+                if (res.IsSuccessStatusCode) // If Success
+                {
+                    var result = await res.Content.ReadAsAsync<string>();
+                    Console.WriteLine( "{0}\n", result);
+                    Console.ReadLine();
+                }
+                else //incorrect input
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Incorrect input. Please try again");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ MessageConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+
+namespace MessageConsole
+{
+    class Program
+    {
+        /// <summary>
+        /// Using HttpClient to call WebAPI method
+        /// Valid input is 1. any other input should return error message.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            HttpClient myConsole = new HttpClient();
+            string apiURL = "http://localhost:6951/";
+            myConsole.BaseAddress = new Uri(apiURL);
+            myConsole.DefaultRequestHeaders.Accept.Clear();
+            myConsole.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            int id = 1;
+            ConsumeAPI api = new ConsumeAPI();
+            api.HelloWorldAPIGet(myConsole, id).Wait();
+        }
+    }
+}
